7b8c6985a26c1c64ae56d41c58829bc1511e8e41
Sergey Zubakha <qasergeyzubakha@yandex.ru>
2023-06-08 17:00:00 +0300

    fix

--- src/test/java/selenide/SoftAssertionsTest.java
+++ src/test/java/selenide/SoftAssertionsTest.java
@@ -1,10 +1,8 @@
 package selenide;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.innerText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -12,10 +10,8 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class SoftAssertionsTest {
 
-
     @Test
     void checkCodeForJunit() {
-        Configuration.holdBrowserOpen = true;
 
 //        - Откройте страницу Selenide в Github
         open("https://github.com/selenide/selenide");
@@ -37,19 +33,6 @@ public class SoftAssertionsTest {
                 "    $(\"#second\").should(visible).click();\n" +
                 "  }\n" +
                 "}"));
-        //$(".markdown-body").$("div", 4).shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
-//                "class Tests {\n" +
-//                "  @Test\n" +
-//                "  void test() {\n" +
-//                "    Configuration.assertionMode = SOFT;\n" +
-//                "    open(\"page.html\");\n" +
-//                "\n" +
-//                "    $(\"#first\").should(visible).click();\n" +
-//                "    $(\"#second\").should(visible).click();\n" +
-//                "  }\n" +
-//                "}"));
-//         тут вопрос, почему по индексу 4 тест сверяет текст не с тем дивом, что я предплаоагаю? (с индексом 8 тест работает)
-//         ведь такая запись $("h1").$("div") должна искать дочерний элемент первого уровня вложенности
-//         а не дивы внутри дивов
+
     }
 }
